b9afd9a3b9c5a72de7cd2923ff3a77da387b4870
qq1429295246 <1429295246@qq.com>
2019-05-21 23:16:30 +0800

    lcd

---
+++ main/main.js
@@ -0,0 +1,9 @@
+module.exports = function main(a) {
+    if(a == "910")
+		return "._. ... ._.\n|_| ..| |.|\n..| ..| |_|\n";
+	if(a == "256")
+		return "._. ._. ._.\n._| |_. |_.\n|_. ._| |_|\n";
+	if(a == "7")
+		return "._.\n..|\n..|\n";
+		
+};
